849ee0cf6799785bdf3868cac1e3d4c599206f4e
amagicmuffin <2014wcheng@gmail.com>
2022-05-27 13:36:50 -0700

    backend improvements and mobile viewport thing

--- docs/script.js
+++ docs/script.js
@@ -1,13 +1,1 @@
-quotes = [
-    ["http://backrooms-wiki.wikidot.com/the-red-knight", "A broken ring of mail"],
-    ["http://backrooms-wiki.wikidot.com/the-red-knight", "erstwhile"],
-    ["http://backrooms-wiki.wikidot.com/the-red-knight", "a finger of an arm of a body that died eons ago."],
-];
-
-
-
-window.onload = function() {
-    quote = quotes[Math.floor(Math.random()*quotes.length)];
-    
-document.getElementById("quote").innerHTML += `<a href=\"${quote[0]}\" target="_blank" rel="noopener noreferrer">${quote[1]}</a>`;
-};
+// generic script for all pages
