0d9e0dd1add04d64d76abaf31754e239bfd7b472
amuffin <62270558+amagicmuffin@users.noreply.github.com>
2025-02-10 20:34:29 -0500

    fix screen flash dark mode

--- docs/darkmode.js
+++ docs/darkmode.js
@@ -1,17 +1,17 @@
 function checkDarkmode() {
 	console.log("got here");
-	var r = document.querySelector(':root');
-	var bgColor = getComputedStyle(r).getPropertyValue("--bg");
+	let root = document.documentElement.style;
+	// let bgColor = getComputedStyle(root).getPropertyValue("--bg");
 
 	console.log("cookie is " + document.cookie);
 	if(document.cookie == "darkmode=On") {
 		console.log("switch TO DARK")
-		r.style.setProperty('--bg', '#20384B');
-		r.style.setProperty('--text', '#E1F2FF');
+		root.setProperty('--bg', '#20384B');
+		root.setProperty('--text', '#E1F2FF');
 	} else {
 		console.log("switch TO LIGHT")
-		r.style.setProperty('--bg', 'white');
-		r.style.setProperty('--text', 'black');
+		root.setProperty('--bg', 'white');
+		root.setProperty('--text', 'black');
 	}
 }
 
