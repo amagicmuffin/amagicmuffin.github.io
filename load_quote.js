4b569039b30e7a91666941bbc577efe8e5d9ad19
amagicmuffin <amagicmuffin@users.noreply.github.com>
2025-02-25 21:00:25 +0000

    deploy: 18e96d9579df6ffe437fc14e056615d4df528ee7

---
+++ load_quote.js
@@ -0,0 +1,14 @@
+quotes = [
+    ["https://backrooms-wiki.wikidot.com/entity-71", "A broken ring of mail"],
+    ["https://backrooms-wiki.wikidot.com/entity-71", "erstwhile"],
+    ["https://backrooms-wiki.wikidot.com/entity-71", "a finger of an arm of a body that died eons ago."],
+    ["https://www.google.com/search?q=Adib+Sin+-+To+Eternity", "you were dancing to a thousand stars"],
+];
+
+
+
+window.onload = function() {
+    quote = quotes[Math.floor(Math.random()*quotes.length)];
+    
+    document.getElementById("quote").innerHTML += `<a href=\"${quote[0]}\" target="_blank" rel="noopener noreferrer">${quote[1]}</a>`;
+};
